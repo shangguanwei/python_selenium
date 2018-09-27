05b5e91dd39e08208aa0c690259fb6cef26a0d10
sgw <384840894@qq.com>
2018-09-27 16:18:38 +0800

    modularization login test

--- python_selenium/login test.py
+++ python_selenium/login test.py
@@ -1,17 +1,58 @@
 import time
 from selenium import webdriver
-d = webdriver.Chrome()
-d.get('http://baihu.680000cun.com/')
-time.sleep(5)
-ele_name = d.find_element_by_id('UsernameOrPhoneNumber')
-ele_pwd = d.find_element_by_id('Password')
-ele_login = d.find_element_by_css_selector('button[type="submit"]')
-ele_name.send_keys('sgw')
-ele_pwd.send_keys('123456')
-ele_login.click()
-time.sleep(5)
-ele_ptyw = d.find_element_by_xpath('//*[@id="sidebar"]/ul/li[1]/a/span')
-ele_ptyw.click()
-time.sleep(5)
-ele_zzgl = d.find_element_by_xpath('//*[@id="sidebar"]/ul/li[1]/ul/li[1]/a')
-ele_zzgl.click()
+from selenium.webdriver.support.ui import WebDriverWait
+
+#打开浏览器
+def openBrowser():
+	webdriver_handle = webdriver.Chrome()
+	return webdriver_handle
+
+#打开网址
+def openUrl(handle,url):
+	handle.get(url)
+	handle.maximize_window
+
+def findElement(handle,arg):
+	ele_name = handle.find_element_by_id(arg['userid'])
+	ele_pwd = handle.find_element_by_id(arg['pwdid'])
+	ele_logbtn = handle.find_element_by_id(arg['logbtnid'])
+	return ele_name,ele_pwd,ele_logbtn
+
+def sendVals(eletuple,arg):
+	listkey = ['uname','pwd']
+	i = 0
+	for key in listkey:
+		eletuple[i].send_keys('')
+		eletuple[i].clear()
+		eletuple[i].send_keys(arg[key])
+		i+=1
+	print (eletuple[2].text)
+	eletuple[2].click()
+
+def checkResult(handle):
+	try:
+		alert = handle.switch_to_alert()
+		alert.accept()
+		print('Accont or pwd error!')
+	except:
+		print('login success!')
+	
+def login_test(ele_dict,user_list):
+	d = openBrowser()
+	openUrl(d,ele_dict['url'])
+	time.sleep(5)
+
+	ele_tuple = findElement(d,ele_dict)
+	for arg in user_list:
+		sendVals(ele_tuple,arg)
+		checkResult(d)
+		time.sleep(5)
+
+if __name__ == '__main__':
+
+	url = 'http://baihu.680000cun.com/'
+	accont = 'sgw'
+	pwd = '1234567'
+	ele_dict = {'url':url,'userid':'UsernameOrPhoneNumber','pwdid':'Password','logbtnid':'submit_button','uname':accont,'pwd':pwd}
+	user_list = [{'uname':accont,'pwd':pwd}]
+	login_test(ele_dict,user_list)
